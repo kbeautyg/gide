4f2abebb62571fbe63c534776b9a2b44b27370d6
Sharp <aiservisy@yandex.ru>
2025-10-04 12:08:02 +0300

    Remove DeviseResponder from Devise config (not needed)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,6 @@ Devise.setup do |config|
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
-  config.responder = DeviseResponder
+  # config.responder = DeviseResponder # TODO: Create custom responder if needed
 end
 
