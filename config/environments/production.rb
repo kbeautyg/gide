eb16d2cdbe1e3ed670ce193e03574f49a9a86c45
Sharp <aiservisy@yandex.ru>
2025-10-04 21:13:38 +0300

    Enable assets compilation in production for Railway deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.compile = false
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RAILWAY_ENVIRONMENT'].present?
+  config.assets.compile = true  # Enable on-demand compilation for Railway
   config.active_storage.service = :local
   
   config.log_level = :info
