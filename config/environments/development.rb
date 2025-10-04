2e3f1c54c4a54c694ef8d873ffd461b83a7b2d80
Sharp <aiservisy@yandex.ru>
2025-10-04 12:16:07 +0300

    Allow Railway hosts in production and development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,5 +29,8 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
   config.assets.quiet = true
+  
+  # Allow all hosts in development
+  config.hosts.clear
 end
 
