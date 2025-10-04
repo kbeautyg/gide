79cd43355453dc307d62cf371d614a35910272a1
Sharp <aiservisy@yandex.ru>
2025-10-04 21:21:16 +0300

    Add assets initializer and enable static file serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,9 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV['RAILWAY_ENVIRONMENT'].present?
+  config.public_file_server.enabled = true  # Serve static files in Railway
   config.assets.compile = true  # Enable on-demand compilation for Railway
+  config.assets.digest = true
   config.active_storage.service = :local
   
   config.log_level = :info
