65499b65a93a1a7dcb23ed6edd08903c328eea51
Sharp <aiservisy@yandex.ru>
2025-10-04 12:06:22 +0300

    Fix Devise config: use stretches instead of stretching_cost

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage = [:http_auth]
-  config.stretching_cost = Rails.env.test? ? 1 : 12
+  config.stretches = Rails.env.test? ? 1 : 12
   config.reconfirmable = true
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 6..128
